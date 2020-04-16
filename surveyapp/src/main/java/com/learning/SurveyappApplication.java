697035584fac0ee2fd02b43b9fa1147d7ea627ae
preet <preet@192.168.1.106>
2020-04-16 14:22:35 +0400

    First commit

---
+++ surveyapp/src/main/java/com/learning/SurveyappApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyappApplication.class, args);
+	}
+
+}
